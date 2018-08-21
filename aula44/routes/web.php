e4c4c7c78fc30cb4cd182b918bb2fba0d39ed493
Wagner Gonçalves <wagnergmendess@icloud.com>
2018-08-20 22:24:01 -0300

    Aula 44

--- aula44/routes/web.php
+++ aula44/routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//no shell -> php artisan make:auth
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
